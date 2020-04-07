dc0f24e57a866b7de3e6d5ac2190a18349f85ef4
bbradforddesign <bbradforddesign@gmail.com>
2020-04-07 13:19:07 -0500

    Added Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,46 @@
 import React from "react";
 import "./styles/App.css";
 
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 //import our components
 import UserInput from "./UserInput";
 
+const NavMenu = () => {
+  return (
+    <div>
+      <Link to="/">
+        <button>Home</button>
+      </Link>
+      <Link to="/about">
+        <button>About</button>
+      </Link>
+    </div>
+  );
+};
+
+const About = () => {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+};
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello, header</h1>
-      </header>
-      <UserInput />
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <h1>Hello, header</h1>
+          <NavMenu />
+        </header>
+        <div>
+          <Route exact path="/" component={UserInput} />
+          <Route exact path="/about" component={About} />
+        </div>
+      </div>
+    </Router>
   );
 }
 
