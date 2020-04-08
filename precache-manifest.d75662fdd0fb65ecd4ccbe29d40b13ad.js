41c41667c4ab5d77eaf1446eb9424c76c93c786f
bbradforddesign <bbradforddesign@gmail.com>
2020-04-08 15:53:45 -0500

    Updates

---
+++ precache-manifest.d75662fdd0fb65ecd4ccbe29d40b13ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd18a77d043dc865289b927c2dcca0ce",
+    "url": "/wyco-housing-help/index.html"
+  },
+  {
+    "revision": "e086ffd6b88b05ea7ac8",
+    "url": "/wyco-housing-help/static/css/main.18ee4fb5.chunk.css"
+  },
+  {
+    "revision": "05955cbd35aaec692d99",
+    "url": "/wyco-housing-help/static/js/2.570697b8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/wyco-housing-help/static/js/2.570697b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e086ffd6b88b05ea7ac8",
+    "url": "/wyco-housing-help/static/js/main.f05ca08d.chunk.js"
+  },
+  {
+    "revision": "584d1d342996d9303d19",
+    "url": "/wyco-housing-help/static/js/runtime-main.61d4f90a.js"
+  }
+]);
